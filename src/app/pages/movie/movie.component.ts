5d945351f5a2ad568a403981880c14d1b0c4f1b8
derenjavadyan <derenjavadyan@gmail.com>
2022-07-04 18:40:05 +0400

    009 Movie Trailers Service

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Movie } from 'src/app/models/movie';
+import { Movie, MovieVideo } from '../../models/movie';
 import { MoviesService } from 'src/app/services/movies.service';
 import { IMAGES_SIZES } from '../../constants/images-sizes';
 
@@ -11,6 +11,7 @@ import { IMAGES_SIZES } from '../../constants/images-sizes';
 })
 export class MovieComponent implements OnInit {
   movie!: Movie;
+  movieVideos!: MovieVideo[];
   imagesSizes = IMAGES_SIZES;
 
   constructor(private route: ActivatedRoute, private moviesService: MoviesService) {}
@@ -18,12 +19,19 @@ export class MovieComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(({ id }) => {
       this.getMovie(id);
+      this.getMovieVideos(id);
     });
   }
 
   getMovie(id: string) {
     this.moviesService.getMovie(id).subscribe((movieData) => {
       this.movie = movieData;
+    });
+  }
+
+  getMovieVideos(id: string) {
+    this.moviesService.getMovieVideo(id).subscribe((movieVideoData) => {
+      this.movieVideos = movieVideoData;
     });
   }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Movie, MovieDto } from '../models/movie';
+import { Movie, MovieDto, MovieVideoDto } from '../models/movie';
 import { of, switchMap } from 'rxjs';
 
 @Injectable({
@@ -16,6 +16,14 @@ export class MoviesService {
     return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`).pipe(
       switchMap((res) => {
         return of(res.results.slice(0, count));
+      })
+    );
+  }
+
+  getMovieVideo(id: string) {
+    return this.http.get<MovieVideoDto>(`${this.baseUrl}/movie/${id}/videos?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results);
       })
     );
   }
